762891097c5e447b63b62ca35146350e80ad3f9a
Qusbell <01024072245a@gmail.com>
2026-05-16 13:44:44 +0900

    dd

---
+++ Assets/_Project/Scripts/Core/DelayEvent.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class DelayEvent
+{
+    private int _currentID = 0;
+
+    public void Invoke(float delay, System.Action callback)
+    {
+        InvokeAsync(++_currentID, delay, callback);
+    }
+
+    public void Cancel()
+    {
+        _currentID++;
+    }
+
+    private async void InvokeAsync(int id, float delay, System.Action callback)
+    {
+        await System.Threading.Tasks.Task.Delay((int)(delay * 1000));
+
+        if (id == _currentID)
+        { callback?.Invoke(); }
+    }
+
+}
